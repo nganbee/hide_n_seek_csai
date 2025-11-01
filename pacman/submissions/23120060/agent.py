feeb3a6adca6f017b69b977d32435018cadb70ea
nganbee <ngankimm510@gmail.com>
2025-11-01 23:49:44 +0700

    Remove func in Ghost Agent

--- pacman/submissions/23120060/agent.py
+++ pacman/submissions/23120060/agent.py
@@ -211,8 +211,8 @@ class GhostAgent(BaseGhostAgent):
             # move ghost randomly
             ghost_moves = self._get_all_valid_moves(ghost_pos, map_state)
             if ghost_moves:
-                best_ghost_move = self._get_move_away(ghost_moves, ghost_pos, pacman_pos)
-                ghost_pos = self._get_new_pos(ghost_pos, best_ghost_move)
+                random_ghost_move = random.choice(ghost_moves)
+                ghost_pos = self._get_new_pos(ghost_pos, random_ghost_move)
             
             # check position again
             if self._get_mahattan_distance(ghost_pos, pacman_pos):
@@ -226,20 +226,6 @@ class GhostAgent(BaseGhostAgent):
                 
         final_pos = self._evaluate_position(ghost_pos, pacman_pos, map_state)
         return max_depth + final_pos
-    
-    def _get_move_away(self, moves, current_pos, target_pos):
-        max_dist = -1.0
-        best_move = Move.STAY
-        
-        for move in moves:
-            new_pos = self._get_new_pos(current_pos, move)
-            dist = self._get_mahattan_distance(new_pos, target_pos)
-            
-            if dist > max_dist:
-                max_dist = dist
-                best_move = move
-                
-        return best_move
     
     def _get_move_towards(self, moves, current_pos, target_pos):
         min_dist = float('inf')
